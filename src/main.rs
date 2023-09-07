29052f86222d04f1ea8b825bda6b41e910849387
= <mathiasandreasen91@gmail.com>
2023-09-07 23:46:30 +0200

    remove commented out variable

--- src/main.rs
+++ src/main.rs
@@ -19,19 +19,15 @@ fn get_resolution() -> (i32, i32) {
     let toml_content = fs::read_to_string("config.toml");
 
     match toml_content {
-        Ok(parsed) => {
-            //let parsed_toml = parsed.parse::<Value>();
-            match parsed.parse::<Value>() {
-                Ok(parsed_toml) => {
-                    // Access variables from the TOML
-                    let width = parsed_toml["resolution"]["width"].as_integer().unwrap();
-                    let height = parsed_toml["resolution"]["height"].as_integer().unwrap();
+        Ok(parsed) => match parsed.parse::<Value>() {
+            Ok(parsed_toml) => {
+                let width = parsed_toml["resolution"]["width"].as_integer().unwrap();
+                let height = parsed_toml["resolution"]["height"].as_integer().unwrap();
 
-                    return (width.try_into().unwrap(), height.try_into().unwrap());
-                }
-                Err(_) => println!("Failed to parse config file."),
+                return (width.try_into().unwrap(), height.try_into().unwrap());
             }
-        }
+            Err(_) => println!("Failed to parse config file."),
+        },
         Err(_) => println!("Failed to read config file, defaulting to 1280x720 resolition"),
     }
 
