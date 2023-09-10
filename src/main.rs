a69a9295cc455f5678972012732ea24568cb85f7
rfaa <rfaa@rfaa.se>
2023-09-10 10:36:42 +0000

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -15,39 +15,51 @@ mod messages;
 mod states;
 mod world;
 
-fn get_resolution() -> (i32, i32) {
-    let toml_content = fs::read_to_string("config.toml");
-
-    match toml_content {
-        Ok(parsed) => match parsed.parse::<Value>() {
-            Ok(parsed_toml) => {
-                let width = parsed_toml["resolution"]["width"].as_integer();
-                let height = parsed_toml["resolution"]["height"].as_integer();
-
-                match (width, height) {
-                    (Some(width), Some(height)) => {
-                        let width: Result<i32, _> = width.try_into();
-                        let height: Result<i32, _> = height.try_into();
-
-                        match (width, height) {
-                            (Ok(width), Ok(height)) => return (width, height),
-                            _ => println!("Failed to parse resolution"),
-                        }
-                    }
-                    _ => println!("Failed to parse resolution"),
-                }
-            }
-            Err(_) => println!("Failed to parse config file."),
-        },
-        Err(_) => println!("Failed to read config file, defaulting to 1280x720 resolition"),
-    }
-
-    (1280, 768)
-}
-
 fn main() {
     let (width, height) = get_resolution();
     let (mut rh, rt) = raylib::init().size(width, height).title("fejd").build();
     let mut engine = Engine::new();
     engine.run(&mut rh, &rt);
+}
+
+fn get_resolution() -> (i32, i32) {
+    // TODO: if we add more configs, consider moving parsing file to its own function
+    let default = (1280, 768);
+
+    let config = match fs::read_to_string("config.toml") {
+        Ok(config) => config,
+        Err(_) => {
+            println!("Failed to read config.toml file");
+            return default;
+        }
+    };
+
+    let config = match config.parse::<Value>() {
+        Ok(config) => config,
+        Err(_) => {
+            println!("Failed to parse config.toml file");
+            return default;
+        }
+    };
+
+    let (width, height) = (
+        config["resolution"]["width"].as_integer(),
+        config["resolution"]["height"].as_integer(),
+    );
+
+    let (width, height) = match (width, height) {
+        (Some(width), Some(height)) => (width.try_into(), height.try_into()),
+        _ => {
+            println!("Failed to parse resolution");
+            return default;
+        }
+    };
+
+    match (width, height) {
+        (Ok(width), Ok(height)) => (width, height),
+        _ => {
+            println!("Failed to read resolution");
+            default
+        }
+    }
 }
